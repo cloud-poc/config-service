f5dc3612470cea55fff69b537ae7dd73c1ee096f
Jamie Zhang <akjamie.zhang@gmail.com>
2019-06-10 14:22:04 +0800

    update config

--- src/main/java/org/akj/springboot/Application.java
+++ src/main/java/org/akj/springboot/Application.java
@@ -2,6 +2,7 @@ package org.akj.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 	
 	public static void main(String[] args) {
