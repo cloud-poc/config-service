657caf363eb4a07d7ee81806dffa4eab3a51c1a5
Jamie Zhang <akjamie.zhang@outlook.com>
2020-11-29 10:33:52 +0800

    update to remove the eureka related to adapt for k8s

--- src/main/java/org/akj/springboot/Application.java
+++ src/main/java/org/akj/springboot/Application.java
@@ -1,9 +1,12 @@
 package org.akj.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import java.util.TimeZone;
 
 /**
  * @author Jamie Y L Zhang
@@ -11,10 +14,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @EnableConfigServer
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class Application {
 	
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(Application.class, args);
 	}
 	
